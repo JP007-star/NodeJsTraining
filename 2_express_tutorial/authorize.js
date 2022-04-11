b1ced1021f6a8e42e9b30faeae154b8df053839c
JP007-Star <justice_of_peace@yahoo.com>
2022-04-11 21:43:54 +0530

    final changes

---
+++ 2_express_tutorial/authorize.js
@@ -0,0 +1,12 @@
+const authorize = (req, res, next) => {
+    const { user } = req.query
+    console.log(req.query);
+    if (user == 'john') {
+        req.user = { name: 'John', id: 1 }
+        next()
+    } else {
+        res.status(401).send('Unauthorized')
+    }
+}
+
+module.exports = authorize
