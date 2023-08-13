2d2bd71dfdf0f5f4ef68413e80e3d5dc2d080be2
JP007-Star <justice_of_peace@yahoo.com>
2023-08-13 20:25:02 +0530

    email

--- email/email.js
+++ email/email.js
@@ -8,17 +8,41 @@ var transporter = nodemailer.createTransport({
   }
 });
 
-var mailOptions = {
-  from: 'jayaprasad.jp.m@gmail.com',
-  to: 'justice_of_peace@yahoo.com',
-  subject: 'Sending Email using Node.js',
-  text: 'That was easy!'
-};
 
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
-});
+
+
+
+
+var express = require('express')
+var app = express()
+
+app.get('/email', function (req, res) {
+  
+  var email = req.query.email;
+  var subject=req.query.subject;
+  var message = req.query.message;
+ 
+  console.log(email);
+  var mailOptions = {
+    from: 'jayaprasad.jp.m@gmail.com',
+    to: email,
+    subject: subject,
+    text: message
+  };
+  
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+  res.send('Simple Example of routes!');
+})
+
+
+
+
+app.listen(3001, function () {
+  console.log('Server is listening at 3000')
+})
